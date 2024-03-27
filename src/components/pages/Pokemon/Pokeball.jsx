e91e31445f871cd182239b99e846ee9fc6652617
Ahmed Jabai <ahmed54.aj@gmail.com>
2024-03-27 19:45:26 -0400

    changed again

--- src/components/pages/Pokemon/Pokeball.jsx
+++ src/components/pages/Pokemon/Pokeball.jsx
@@ -33,8 +33,8 @@ const texture = loader.load([
 scene.background = texture;
 // 2) load the model
 const pokeball_loader = new GLTFLoader();
-let ball
-pokeball_loader.load( 'object.json', function (gltf) {
+let ball;
+pokeball_loader.load( 'object.glb', function (gltf) {
     ball = gltf.scene;
     ball.scale.set(1,  1,  1);
     ball.position.x = -5.4;
